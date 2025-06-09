1084e85ef9d62643fb17f22d6d82159257d4c318
vikas-gits-good <vikas.c.conappa@protonmail.com>
2025-06-09 20:05:54 +0530

    Final Deployment 2

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ async def predict_route(request: Request = None, file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    app_run(app, host="0.0.0.0", port=8000)
+    app_run(app, host="0.0.0.0", port=8080)
